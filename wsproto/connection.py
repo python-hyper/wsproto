14b55a89bd8f2aba6216c905a8881f1fa9b129a9
Benno Rice <benno@jeamland.net>
2017-06-02 14:46:01 -0700

    Fix up some corner cases in upgrade message handling.

--- wsproto/connection.py
+++ wsproto/connection.py
@@ -239,11 +239,15 @@ class WSConnection(object):
             event = self._upgrade_connection.next_event()
             if event is h11.NEED_DATA:
                 break
-            elif self.client and isinstance(event, h11.InformationalResponse):
+            elif self.client and isinstance(event, (h11.InformationalResponse,
+                                                    h11.Response)):
                 data = self._upgrade_connection.trailing_data[0]
                 return self._establish_client_connection(event), data
             elif not self.client and isinstance(event, h11.Request):
                 return self._process_connection_request(event), None
+            else:
+                return ConnectionFailed(CloseReason.PROTOCOL_ERROR,
+                                        "Bad HTTP message")
 
         self._incoming = b''
         return None, None
