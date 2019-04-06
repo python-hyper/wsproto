a7abcc5a9f7ad126668afb0cc9932da08c87f40f
pgjones <philip.graham.jones@googlemail.com>
2019-04-06 18:27:18 +0100

    Following the release of 0.14.0 bump to +dev

--- wsproto/__init__.py
+++ wsproto/__init__.py
@@ -8,7 +8,7 @@ A WebSocket implementation.
 from .connection import ConnectionType
 from .handshake import H11Handshake
 
-__version__ = "0.14.0"
+__version__ = "0.14.0+dev"
 
 
 class WSConnection(object):
