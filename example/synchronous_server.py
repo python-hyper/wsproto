18756d5d81493c1d1d2521afe106263bcc7632dc
Ran Benita <ran234@gmail.com>
2019-07-06 17:42:36 +0300

    Reformat example with Black
    To be consistent with the rest of the project. Also checked in CI.

--- example/synchronous_server.py
+++ example/synchronous_server.py
@@ -1,27 +1,34 @@
-'''
+"""
 This server reads a message from a WebSocket, and sends the reverse string in a
 response message. It can only handle one client at a time. This is a very bad
 implementation of a server! It is only intended to demonstrate how to use
 wsproto.
-'''
+"""
 
 import socket
 import sys
 
 from wsproto import ConnectionType, WSConnection
-from wsproto.events import AcceptConnection, CloseConnection, Message, Ping, Request, TextMessage
-
+from wsproto.events import (
+    AcceptConnection,
+    CloseConnection,
+    Message,
+    Ping,
+    Request,
+    TextMessage,
+)
 
 MAX_CONNECTS = 5
 RECEIVE_BYTES = 4096
 
+
 def main():
-    ''' Run the server. '''
+    """Run the server."""
     try:
         ip = sys.argv[1]
         port = int(sys.argv[2])
     except (IndexError, ValueError):
-        print('Usage: {} <BIND_IP> <PORT>'.format(sys.argv[0]))
+        print("Usage: {} <BIND_IP> <PORT>".format(sys.argv[0]))
         sys.exit(1)
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -31,18 +38,18 @@ def main():
 
     try:
         while True:
-            print('Waiting for connection...')
+            print("Waiting for connection...")
             (stream, addr) = server.accept()
-            print('Client connected: {}:{}'.format(addr[0], addr[1]))
+            print("Client connected: {}:{}".format(addr[0], addr[1]))
             handle_connection(stream)
             stream.shutdown(socket.SHUT_WR)
             stream.close()
     except KeyboardInterrupt:
-        print('Received SIGINT: shutting down…')
+        print("Received SIGINT: shutting down…")
 
 
 def handle_connection(stream):
-    '''
+    """
     Handle a connection.
 
     The server operates a request/response cycle, so it performs a synchronous
@@ -53,46 +60,49 @@ def handle_connection(stream):
     3) Send data from wsproto to network
 
     :param stream: a socket stream
-    '''
+    """
     ws = WSConnection(ConnectionType.SERVER)
     running = True
 
     while running:
         # 1) Read data from network
         in_data = stream.recv(RECEIVE_BYTES)
-        print('Received {} bytes'.format(len(in_data)))
+        print("Received {} bytes".format(len(in_data)))
         ws.receive_data(in_data)
 
         # 2) Get new events and handle them
-        out_data = b''
+        out_data = b""
         for event in ws.events():
             if isinstance(event, Request):
                 # Negotiate new WebSocket connection
-                print('Accepting WebSocket upgrade')
+                print("Accepting WebSocket upgrade")
                 out_data += ws.send(AcceptConnection())
             elif isinstance(event, CloseConnection):
                 # Print log message and break out
-                print('Connection closed: code={}/{} reason={}'.format(
-                    event.code.value, event.code.name, event.reason))
+                print(
+                    "Connection closed: code={}/{} reason={}".format(
+                        event.code.value, event.code.name, event.reason
+                    )
+                )
                 out_data += ws.send(event.response())
                 running = False
             elif isinstance(event, TextMessage):
                 # Reverse text and send it back to wsproto
-                print('Received request and sending response')
+                print("Received request and sending response")
                 out_data += ws.send(Message(data=event.data[::-1]))
             elif isinstance(event, Ping):
                 # wsproto handles ping events for you by placing a pong frame in
                 # the outgoing buffer. You should not call pong() unless you want to
                 # send an unsolicited pong frame.
-                print('Received ping and sending pong')
+                print("Received ping and sending pong")
                 out_data += ws.send(event.response())
             else:
-                print('Unknown event: {!r}'.format(event))
+                print("Unknown event: {!r}".format(event))
 
         # 4) Send data from wsproto to network
-        print('Sending {} bytes'.format(len(out_data)))
+        print("Sending {} bytes".format(len(out_data)))
         stream.send(out_data)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
