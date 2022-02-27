4580e161c88bfbbaae5bf5fc0baefcc79ab45535
Thomas Kriechbaumer <thomas@kriechbaumer.name>
2022-02-27 10:40:41 +0100

    Following the release of 1.1.0 bump to +dev

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,6 +1,12 @@
 Release History
 ===============
 
+Unreleased
+----------
+
+- <ToDo: add new entries here>
+
+
 1.1.0 (2022-02-27)
 ------------------
 
--- src/wsproto/__init__.py
+++ src/wsproto/__init__.py
@@ -11,7 +11,7 @@ from .events import Event
 from .handshake import H11Handshake
 from .typing import Headers
 
-__version__ = "1.1.0"
+__version__ = "1.1.0+dev"
 
 
 class WSConnection:
